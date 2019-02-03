f0c8c8868c660dbadd4a386107b91e366556c5d1
Thomas Chappel <thomas.chappel4@gmail.com>
2019-02-03 11:20:32 +0100

    fix navbar layout and move navbar scripts to js file

---
+++ src/scripts/navbar.js
@@ -0,0 +1,15 @@
+const sidenav = document.getElementById("sidenav");
+function closeSidenav() {
+  sidenav.classList.remove("open");
+  sidenav.classList.add("closed");
+  setTimeout(function() {
+    sidenav.classList.remove("closed");
+  }, 500);
+}
+function toggleSidenav() {
+  if (sidenav.classList.contains("open")) {
+    closeSidenav();
+  } else if (!sidenav.classList.contains("open")) {
+    sidenav.classList.add("open");
+  }
+}
